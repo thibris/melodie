208dc5ce2360feaa16cd0ec8dea0028bca126475
thibris <148239732+thibris@users.noreply.github.com>
2024-09-21 00:18:54 +0200

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,17 +1,20 @@
 <?php
 if(isset($_GET['archive']))
 {
-	#$filename = '/var/www/html/'.$_GET['archive'];
-	rename('/var/www/html/'.$_GET['archive'], '/var/www/html/archive/'.$_GET['archive']);
-	#unlink($filename);
+        rename('/var/www/html/'.$_GET['archive'], '/var/www/html/archive/'.$_GET['archive']);
+}
+if(isset($_GET['remove']))
+{
+        $filename = '/var/www/html/'.$_GET['remove'];
+        unlink($filename);
 }
 if(isset($_GET['cut']))
 {
         echo "filename: ".$_GET['cut']."<br>";
         $filename=$_GET['cut'];
-	$command='ffmpeg -i '.$filename.' -acodec copy -ss 00:08:10.000 -t 01:05:00.000 cut_'.$filename;
+        $command='ffmpeg -i '.$filename.' -acodec copy -ss 00:08:10.000 -t 01:05:00.000 cut_'.$filename;
         $output=null;
-	system($command, $output);
-	rename('/var/www/html/'.$filename, '/var/www/html/originals/'.$filename);
+        system($command, $output);
+        rename('/var/www/html/'.$filename, '/var/www/html/originals/'.$filename);
 }
 ?>
